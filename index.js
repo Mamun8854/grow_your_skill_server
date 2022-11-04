9235b241f0af980f9324d646a4ddaa7b512ca8ce
Mamun8854 <mamunabdullah8854@gmail.com>
2022-11-04 20:50:46 +0600

    courses data added

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ app.get("/", (req, res) => {
   res.send("Learning API running");
 });
 
+const datas = require("./data/data.json");
+app.get("/courses", (req, res) => {
+  res.send(datas);
+});
+
 app.listen(port, () => {
   console.log(`Learning API running on port ${port}`);
 });
