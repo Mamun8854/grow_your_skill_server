9cebb7d67cde44af678dc5b5aacab6aa9623d9ca
Mamun8854 <mamunabdullah8854@gmail.com>
2022-11-04 21:13:41 +0600

    single data added

--- index.js
+++ index.js
@@ -14,6 +14,13 @@ app.get("/courses", (req, res) => {
   res.send(datas);
 });
 
+app.get("/courses/:id", (req, res) => {
+  const id = req.params.id;
+  const data = datas.find((result) => result.id == id);
+  console.log(data);
+  res.send(data);
+});
+
 app.listen(port, () => {
   console.log(`Learning API running on port ${port}`);
 });
